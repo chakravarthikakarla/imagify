43c47e6cdb0a2be8a0dfb30e4f437c4c67bd0c31
chakravarthikakarla <chakravarthirock7@gmail.com>
2025-07-28 21:40:25 +0530

    updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-between gap-4 py-3mt-20">
+    <div className="flex items-center justify-between gap-4 py-3 mt-20">
       <img src={assets.logo} alt="" width={150} />
-<p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @GreatStack.dev |
+<p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @chakri.dev |
 All right reserved.</p>
      <div className="flex gap-2.5">
         <img src={assets.facebook_icon} alt="" width={35} />
